da7252c019c653b06bc6ca20d1995e77c016ad09
robert jakub <rj@project2.pl>
2025-02-15 20:41:47 +0100

    nix-darwin now manages nix-daemon unconditionally

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -4,7 +4,7 @@ in {
   imports = [../base] ++ (fn.scanPaths ./.);
   config = {
     # services.activate-system.enable = true;
-    services.nix-daemon.enable = true;
+    # services.nix-daemon.enable = true;
     system.stateVersion = 4;
   };
 }
