e6d4cf0f436c071ab064b6b1bbd8dc70142315e7
robert jakub <rj@project2.pl>
2025-11-17 10:28:43 +0100

    Update default.nix

--- modules/nixos/cpu/aarch64/default.nix
+++ modules/nixos/cpu/aarch64/default.nix
@@ -1,7 +1,9 @@
-{lib, ...}: let
+{ lib, ... }:
+let
   inherit (lib) mkDefault;
-in {
-  nixpkgs.hostPlatform = mkDefault "aarch64-linux";
+in
+{
+  # nixpkgs.hostPlatform = mkDefault "aarch64-linux";
 
   # workaround for https://github.com/NixOS/nixpkgs/issues/344963
   boot.initrd.systemd.tpm2.enable = false;
